442c0a508e5b3f74d812428ac340d3baeab88dd0
Michael Tyson <michael@atastypixel.com>
2012-04-22 19:43:19 +0200

    Added a number of audio buffer list utilities

--- TheAmazingAudioEngine/AEUtilities.c
+++ TheAmazingAudioEngine/AEUtilities.c
@@ -8,15 +8,59 @@
 
 #include "AEUtilities.h"
 
-int ABGetNumberOfFramesInAudioBufferList(AudioBufferList *list, AudioStreamBasicDescription *audioFormat, int *oNumberOfChannels) {
-    int channelCount = audioFormat->mFormatFlags & kAudioFormatFlagIsNonInterleaved ? list->mNumberBuffers : list->mBuffers[0].mNumberChannels;
-    if ( oNumberOfChannels ) {
-        *oNumberOfChannels = channelCount;
+AudioBufferList *AEAllocateAndInitAudioBufferList(AudioStreamBasicDescription *audioFormat, int frameCount) {
+    int numberOfBuffers = audioFormat->mFormatFlags & kAudioFormatFlagIsNonInterleaved ? audioFormat->mChannelsPerFrame : 1;
+    int channelsPerBuffer = audioFormat->mFormatFlags & kAudioFormatFlagIsNonInterleaved ? 1 : audioFormat->mChannelsPerFrame;
+    int bytesPerBuffer = audioFormat->mBytesPerFrame * frameCount;
+    
+    AudioBufferList *audio = malloc(sizeof(AudioBufferList) + (numberOfBuffers-1)*sizeof(AudioBuffer));
+    if ( !audio ) {
+        return NULL;
     }
-    return list->mBuffers[0].mDataByteSize / ((audioFormat->mBitsPerChannel/8) * channelCount);
+    for ( int i=0; i<numberOfBuffers; i++ ) {
+        if ( bytesPerBuffer > 0 ) {
+            audio->mBuffers[i].mData = malloc(bytesPerBuffer);
+            if ( !audio->mBuffers[i].mData ) {
+                for ( int j=0; j<i; j++ ) free(audio->mBuffers[j].mData);
+                free(audio);
+                return NULL;
+            }
+        } else {
+            audio->mBuffers[i].mData = NULL;
+        }
+        audio->mBuffers[i].mDataByteSize = bytesPerBuffer;
+        audio->mBuffers[i].mNumberChannels = channelsPerBuffer;
+    }
+    return audio;
 }
 
-void ABInitAudioBufferList(AudioBufferList *list, int listSize, AudioStreamBasicDescription *audioFormat, void *data, int dataSize) {
+AudioBufferList *AECopyAudioBufferList(AudioBufferList *original) {
+    AudioBufferList *audio = malloc(sizeof(AudioBufferList) + (original->mNumberBuffers-1)*sizeof(AudioBuffer));
+    if ( !audio ) {
+        return NULL;
+    }
+    for ( int i=0; i<original->mNumberBuffers; i++ ) {
+        audio->mBuffers[i].mData = malloc(original->mBuffers[i].mDataByteSize);
+        if ( !audio->mBuffers[i].mData ) {
+            for ( int j=0; j<i; j++ ) free(audio->mBuffers[j].mData);
+            free(audio);
+            return NULL;
+        }
+        audio->mBuffers[i].mDataByteSize = original->mBuffers[i].mDataByteSize;
+        audio->mBuffers[i].mNumberChannels = original->mBuffers[i].mNumberChannels;
+        memcpy(audio->mBuffers[i].mData, original->mBuffers[i].mData, original->mBuffers[i].mDataByteSize);
+    }
+    return audio;
+}
+
+void AEFreeAudioBufferList(AudioBufferList *bufferList ) {
+    for ( int i=0; i<bufferList->mNumberBuffers; i++ ) {
+        if ( bufferList->mBuffers[i].mData ) free(bufferList->mBuffers[i].mData);
+    }
+    free(bufferList);
+}
+
+void AEInitAudioBufferList(AudioBufferList *list, int listSize, AudioStreamBasicDescription *audioFormat, void *data, int dataSize) {
     list->mNumberBuffers = audioFormat->mFormatFlags & kAudioFormatFlagIsNonInterleaved ? audioFormat->mChannelsPerFrame : 1;
     assert(list->mNumberBuffers == 1 || listSize > (sizeof(AudioBufferList)+sizeof(AudioBuffer)) );
     
@@ -25,4 +69,12 @@ void ABInitAudioBufferList(AudioBufferList *list, int listSize, AudioStreamBasic
         list->mBuffers[0].mData = (char*)data + (i * (dataSize/list->mNumberBuffers));
         list->mBuffers[0].mDataByteSize = dataSize/list->mNumberBuffers;
     }
+}
+
+int AEGetNumberOfFramesInAudioBufferList(AudioBufferList *list, AudioStreamBasicDescription *audioFormat, int *oNumberOfChannels) {
+    int channelCount = audioFormat->mFormatFlags & kAudioFormatFlagIsNonInterleaved ? list->mNumberBuffers : list->mBuffers[0].mNumberChannels;
+    if ( oNumberOfChannels ) {
+        *oNumberOfChannels = channelCount;
+    }
+    return list->mBuffers[0].mDataByteSize / ((audioFormat->mBitsPerChannel/8) * channelCount);
 }
--- TheAmazingAudioEngine/AEUtilities.h
+++ TheAmazingAudioEngine/AEUtilities.h
@@ -13,6 +13,62 @@ extern "C" {
 #endif
 
 /*!
+ * Allocate an audio buffer list and the associated mData pointers.
+ *
+ *  Note: Do not use this utility from within the Core Audio thread (such as inside a render
+ *  callback). It may cause the thread to block, inducing audio stutters.
+ *
+ * @param audioFormat       Pointer to audio format describing audio to be stored in buffer list
+ * @param frameCount        The number of frames to allocate space for (or 0 to just allocate the list structure itself)
+ * @return The allocated and initialised audio buffer list
+ */
+AudioBufferList *AEAllocateAndInitAudioBufferList(AudioStreamBasicDescription *audioFormat, int frameCount);
+
+/*!
+ * Create a copy of an audio buffer list
+ *
+ *  Note: Do not use this utility from within the Core Audio thread (such as inside a render
+ *  callback). It may cause the thread to block, inducing audio stutters.
+ *
+ * @param original          The original AudioBufferList to copy
+ * @return The new, copied audio buffer list
+ */
+AudioBufferList *AECopyAudioBufferList(AudioBufferList *original);
+    
+/*!
+ * Free a buffer list and associated mData buffers
+ *
+ *  Note: Do not use this utility from within the Core Audio thread (such as inside a render
+ *  callback). It may cause the thread to block, inducing audio stutters.
+ */
+void AEFreeAudioBufferList(AudioBufferList *bufferList);
+    
+/*!
+ * Initialize a pre-allocated audio buffer list structure
+ *
+ *  Populates the fields in the given audio buffer list. This utility is useful when
+ *  allocating an audio buffer list on the stack.
+ *
+ *  Sample usage:
+ *  
+ *  @code
+ *  char audioBufferListSpace[sizeof(AudioBufferList)+sizeof(AudioBuffer)];
+ *  AudioBufferList *bufferList = (AudioBufferList*)audioBufferListSpace;
+ *  AEInitAudioBufferList(bufferList, sizeof(audioBufferListSpace), &THIS->_audioFormat, THIS->_audioBytes, kAudioBufferSize);
+ *  @endcode
+ *
+ * @param list          Audio buffer list to initialize
+ * @param listSize      Size of buffer list structure (eg. "sizeof(list)")
+ * @param audioFormat   Pointer to audio format describing audio to be stored in buffer list
+ * @param data          Optional pointer to a buffer to point the mData pointers within buffer list to.
+ *                      If audio format is more than one channel and non-interleaved, the buffer will be
+ *                      broken up into even pieces, one for each channel.
+ * @param dataSize      Size of 'data' buffer, in bytes.
+ */
+void AEInitAudioBufferList(AudioBufferList *list, int listSize, AudioStreamBasicDescription *audioFormat, void *data, int dataSize);
+
+
+/*!
  * Get the number of frames in a buffer list
  *
  *  Calculates the frame count in the buffer list based on the given
@@ -23,31 +79,8 @@ extern "C" {
  * @param oNumberOfChannels If not NULL, will be set to the number of channels of audio in 'list'
  * @return Number of frames in the buffer list
  */
-int ABGetNumberOfFramesInAudioBufferList(AudioBufferList *list, AudioStreamBasicDescription *audioFormat, int *oNumberOfChannels);
+int AEGetNumberOfFramesInAudioBufferList(AudioBufferList *list, AudioStreamBasicDescription *audioFormat, int *oNumberOfChannels);
 
-/*!
- * Initialize an audio buffer list structure
- *
- *  Uses info from the given audio format to populate the fields in the given
- *  audio buffer list.
- *
- *  Sample usage:
- *  
- *  <code>
- *  char audioBufferListSpace[sizeof(AudioBufferList)+sizeof(AudioBuffer)];
- *  AudioBufferList *bufferList = (AudioBufferList*)audioBufferListSpace;
- *  ABInitAudioBufferList(bufferList, sizeof(audioBufferListSpace), &THIS->_audioFormat, THIS->_audioBuffer, kAudioBufferSize);
- *  </code>
- *
- * @param list          Audio buffer list to initialize
- * @param listSize      Size of buffer list structure (eg. "sizeof(list)")
- * @param audioFormat   Pointer to audio format describing audio to be stored in buffer list
- * @param data          Optional pointer to a buffer to point the mData pointers within buffer list to.
- *                      If audio format is stereo and non-interleaved, the second channel will point to the mid-point of the data buffer (data + dataSize/2)
- * @param dataSize      Size of 'data' buffer, in bytes.
- */
-void ABInitAudioBufferList(AudioBufferList *list, int listSize, AudioStreamBasicDescription *audioFormat, void *data, int dataSize);
-    
 #ifdef __cplusplus
 }
 #endif
