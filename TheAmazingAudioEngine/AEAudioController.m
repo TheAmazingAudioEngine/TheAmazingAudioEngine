5023b4efb6366c7f08bfbaec7d84ef6a249019bb
Michael Tyson <michael@atastypixel.com>
2014-08-02 11:36:18 +1000

    Revert "Added a missing "__unsafe_unretained""
    This reverts commit da92cd5ab93d7f40cc7db11d0376bda16f5ceb74.
    
    The __unsafe_unretained shoudn't actually be there, as this is a main thread callback.

--- TheAmazingAudioEngine/AEAudioController.m
+++ TheAmazingAudioEngine/AEAudioController.m
@@ -2278,7 +2278,7 @@ NSTimeInterval AEAudioControllerOutputLatency(__unsafe_unretained AEAudioControl
 }
 
 static void IsInterAppConnectedCallback(void *inRefCon, AudioUnit inUnit, AudioUnitPropertyID inID, AudioUnitScope inScope, AudioUnitElement inElement) {
-    __unsafe_unretained AEAudioController *THIS = (__bridge AEAudioController*)inRefCon;
+    AEAudioController *THIS = (__bridge AEAudioController*)inRefCon;
     if ( THIS->_inputEnabled ) {
         [THIS updateInputDeviceStatus];
     }
